4afe003ffe1c14debbd4d50cea50cb53ac002e6a
uchida189 <perukipoo@icloud.com>
2025-03-11 13:39:04 +0900

    [add]FoodPageを作成

---
+++ app/pages/food/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FoodDetailPage() {
+  return (
+    <div>
+      <h1>Food Detail Page</h1>
+    </div>
+  );
+}
---
+++ app/pages/food/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FoodPage() {
+  return (
+    <div>
+      <h1>Food Page</h1>
+    </div>
+  );
+}
