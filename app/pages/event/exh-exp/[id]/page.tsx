b7eecea85f3474ed5fa163811fcbd2962eb1299e
uchida189 <perukipoo@icloud.com>
2025-03-11 13:34:12 +0900

    [add]eventページを作成

---
+++ app/pages/event/exh-exp/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ExhExpDetailPage() {
+  return (
+    <div>
+      <h1>ExhExp Detail Page</h1>
+    </div>
+  );
+}
---
+++ app/pages/event/plan/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PlanDetailPage() {
+  return (
+    <div>
+      <h1>Plan Detail Page</h1>
+    </div>
+  );
+}
