b5cf5f089bae363ca06b7a6c23c089fd9fd2102a
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-03-15 23:47:39 +0900

    [feat]global.cssのroot設定に--fontの定義を追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className='{${geistSans.variable} ${geistMono.variable} ${notoSansJP.className} ${notoSansJP.variable} ${newTegomin.variable} antialiased'>
+        className={'${geistSans.variable} ${geistMono.variable} ${notoSansJP.className} ${notoSansJP.variable} ${newTegomin.variable} antialiased'}>
         {children}
       </body>
     </html>
