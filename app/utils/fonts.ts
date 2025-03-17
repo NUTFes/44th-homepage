d3052a364680f26480a481cb3799b4b3be62af93
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-03-17 17:29:07 +0900

    [fix]srcを修正

--- app/utils/fonts.ts
+++ app/utils/fonts.ts
@@ -1,13 +1,13 @@
 import localFont from "next/font/local";
 
 export const notoSansJP = localFont({
-  src: "./fonts/NotoSansJP-VariableFont_wght.ttf",
+  src: "../fonts/NotoSansJP-VariableFont_wght.ttf",
   display: 'swap',
   variable: "--font-noto-sans-jp",
 });
 
 export const newTegomin = localFont({
-  src: './fonts/NewTegomin-Regular.ttf',
+  src: '../fonts/NewTegomin-Regular.ttf',
   display: 'swap',
   variable: '--font-new-tegomin',
 });
