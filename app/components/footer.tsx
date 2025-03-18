cd4f5f78af69651e20021214ce7bbc397c4baa27
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-03-18 22:50:17 +0900

    [fix]classNameをまとめた

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -9,13 +9,13 @@ export default function Footer(){
                 <Image src="/icon/Instagram_Glyph_White.svg" alt="instagram-link" width={50} height={50}/>
 
             </div>
-            <div className="flex flex-col justify-center p-4">
-            <div className="text-body2 text-gray text-center whitespace-pre-wrap">協賛企業一覧</div>
-            <div className="text-body2 text-gray text-center whitespace-pre-wrap">アンケートリンク</div>
-            <div className="text-body2 text-gray text-center whitespace-pre-wrap">長岡技術科学大学ホームページ</div>
-            <div className="p-2">
-            <div className="text-body2 text-white text-center whitespace-pre-wrap">〒940-2188</div>
-            <div className="text-body2 text-white text-center whitespace-pre-wrap">新潟県長岡市上富岡町1603-1 長岡技術科学大学</div>
+            <div className="flex flex-col justify-center p-4 text-body2 text-gray text-center whitespace-pre-wrap">
+            <div>協賛企業一覧</div>
+            <div>アンケートリンク</div>
+            <div>長岡技術科学大学ホームページ</div>
+            <div className="p-2 text-white">
+            <div>〒940-2188</div>
+            <div>新潟県長岡市上富岡町1603-1 長岡技術科学大学</div>
             </div>
           </div>
         </footer>
