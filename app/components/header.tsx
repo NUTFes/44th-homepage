827add05b03e1e94288b3961e4d7f378dca317c0
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-03-18 16:15:59 +0900

    [fix]画像のsrcを修正、alt,w,hを設定

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,10 +3,20 @@ export default function Header() {
     return (
       <header className="fixed top-0 left-0 w-full bg-black flex justify-between p-4">
         <div className="flex">
-            <img src="../public/icon/44thlogo.png" />
+            <Image 
+            src="/icon/44thlogo.png"
+            alt="44th_icon"
+            width={40}
+            height={40}
+            />
         </div>
         <div className="flex">
-            <img src="../public/icon/BiMenu.svg" />
+            <Image 
+            src="/icon/BiMenu.svg"
+            alt="menu"
+            width={32}
+            height={32}
+            />
         </div>
       </header>
     );
