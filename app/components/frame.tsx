482e28a6cb0a04f9b6a3801f920f52beba1315e1
akari24320 <24.a.ito.nutfes@gmail.com>
2025-04-16 15:12:37 +0900

    中央揃えにする

--- app/components/frame.tsx
+++ app/components/frame.tsx
@@ -13,18 +13,20 @@ const Frame: React.FC<AttentionFrameProps> = ({
     children,
 }) => {
     return (
-    <div
-        className={`p-0.5 gap-4 border-2 border-main bg-base_back rounded-sm
-        ${w === "big" ? "w-[366px]" : ""}
-        ${w === "small" ? "w-[186px]" : "w-[297px]"}`}>
-        <div
-            className={`w-full bg-base_back border border-main rounded-sm
-            ${pg === "none" ? "" : "p-6"}`}>
-            <div className={`flex flex-col ${pg === "none" ? "" : "gap-4"}`}>
-                {children}
+        <div className="flex justify-center">
+            <div
+                className={`p-0.5 border-2 border-main bg-base_back rounded-sm
+                ${w === "big" ? "w-[366px]" : ""}
+                ${w === "small" ? "w-[186px]" : "w-[297px]"}`}>
+                <div
+                    className={`w-full bg-base_back border border-main rounded-sm
+                    ${pg === "none" ? "" : "p-6"}`}>
+                    <div className={`flex flex-col ${pg === "none" ? "" : "gap-4"}`}>
+                        {children}
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
 );
 };
 
