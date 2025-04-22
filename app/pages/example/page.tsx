677a96d4f026a79fe6f6140c8a59a9cad7ecc477
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-04-23 01:14:40 +0900

    [fix]topページのメモをexampleに移動

---
+++ app/pages/example/page.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import TextStyle from "@/app/components/text_style";
+import Frame from "@/app/components/frame";
+import Icon from "@/public/icon/44thlogo.png"
+import Image from "next/image";
+import Line from "@/app/components/line";
+
+export default function TopPage() {
+  return (
+    <div>
+      <Frame>
+        <TextStyle styleType="title">タイトル</TextStyle>
+          <div>
+          <TextStyle styleType="body1">これは本文です。これは本文です。これは本文です。</TextStyle>
+          <TextStyle styleType="body1_bold_khaki">太字＆カーキ色の本文1</TextStyle>
+          <TextStyle styleType="body1">これは本文です。</TextStyle>
+          </div>
+      </Frame>
+
+      <Frame></Frame>
+
+      <Frame pg="none">
+        <div>
+          <Image
+            src={Icon}
+            alt="44th Logo"
+            width={100}
+            height={100} />
+      </div>
+          <TextStyle styleType="title">タイトル</TextStyle>
+          <TextStyle styleType="body1">これはパディングのないフレーム</TextStyle>
+      </Frame>
+
+      <Frame w="big">
+        <div className="text-center"><TextStyle styleType="title">お知らせ</TextStyle></div>
+          <Line/>
+          <div>
+            <p>
+              <TextStyle styleType="body1_khaki">2025/00/00</TextStyle>
+            </p>
+            <TextStyle styleType="body1">これは本文です。これは本文です。これは本文です。これは本文です。これは本文です。</TextStyle>
+          </div>
+          <div>
+            <p>
+              <TextStyle styleType="body1_khaki">2025/00/00</TextStyle>
+            </p>
+            <TextStyle styleType="body1">これは本文です。これは本文です。これは本文です。これは本文です。これは本文です。</TextStyle>
+            <TextStyle styleType="body1_bold_khaki">太字＆カーキ色の本文1</TextStyle>
+            <TextStyle styleType="body1">これは本文です。</TextStyle>
+          </div>
+      </Frame>
+
+      <h1 className="font-tegomin">Top Page tegomin</h1>
+      <h1 className="font-noto">Top Page noto sans jp</h1>
+      <div className="font-not text-h1">h1</div>
+      <div className="font-not text-h2">h2</div>
+      <div className="font-not text-body1">body1</div>
+      <div className="font-not text-body2">body2</div>
+      <div className="font-not text-body3">body3</div>
+      <div className="text-white bg-black">white</div>
+      <div className="text-black">black</div>
+      <div className="text- bg-base">base</div>
+      <div className="text-main ">main</div>
+      <div className="text-second">second</div>
+      <div className="text-accent">accent</div>
+      <div className="text-gray">gray</div>
+      <div className="text-font_main">font-main</div>
+      <div className="text-font_khaki">font-khaki</div>
+      <div className="text-logo_color">logo-color</div>
+      <div className="text-font_link">font-link</div>
+      <div className="bg-white_back">white-back</div>
+      <div className="bg-base_back">base-back</div>
+      <div className="bg-dark_back">dark-back</div>
+      <div className="shadow_right bg-dark_back text-white">shadow_right</div>
+      <div className="shadow_font">shadow_font</div>
+      <div className="shadow_logo m-4">shadow_logo</div>
+      <div className="shadow_dark m-4">shadow_dark</div>
+      <div className="bg-second shadow_button m-4">shadow_button</div>
+      <TextStyle styleType="title">タイトル</TextStyle>
+      <TextStyle styleType="section_title">セクションタイトル</TextStyle>
+      <TextStyle styleType="section_title_red">
+        赤いセクションタイトル
+      </TextStyle>
+      <TextStyle styleType="body1">本文1</TextStyle>
+      <TextStyle styleType="body1_bold">太字の本文1</TextStyle>
+      <TextStyle styleType="body1_khaki">カーキ色の本文1</TextStyle>
+      <TextStyle styleType="body1_bold_khaki">太字＆カーキ色の本文1</TextStyle>
+      <TextStyle styleType="body2">本文2</TextStyle>
+      <TextStyle styleType="body2_bold">太字の本文2</TextStyle>
+      <TextStyle styleType="body2_khaki">カーキ色の本文2</TextStyle>
+      <TextStyle styleType="body2_bold_khaki">太字＆カーキ色の本文2</TextStyle>
+      <TextStyle styleType="body3">本文3</TextStyle>
+      <TextStyle styleType="body3_khaki">カーキ色の本文3</TextStyle>
+    </div>
+  );
+}
