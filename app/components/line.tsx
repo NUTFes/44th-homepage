b28037a1eb3c67da5a5431bb962174fe804cd01b
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-04-27 19:53:43 +0900

    [add]Lineの色を変更できるようにコンポーネントを修正

--- app/components/line.tsx
+++ app/components/line.tsx
@@ -1,6 +1,13 @@
-const Line = () => {
+
+type LineProps = {
+    className?: string;
+};
+
+const Line: React.FC<LineProps> = ({
+    className,
+}) => {
     return(
-        <div className="border-[1px] border-accent rounded-lg"></div>
+        <div className={`border-[1px] border-accent rounded-lg ${className}`}/>
     );
 }
 export default Line;
