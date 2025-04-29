99ed4dd0e7c06116f4d1809f529d2e370e64f7d3
akari24320 <24.a.ito.nutfes@gmail.com>
2025-04-29 13:51:27 +0900

    APIを用いてお知らせコンポーネントを作成

---
+++ app/components/info.tsx
@@ -0,0 +1,65 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import TextStyle from './text_style';
+
+interface Info {
+    番号: string;
+    投稿日時: string;
+    タイトル: string;
+    内容: string;
+}
+
+const Infos: React.FC = () => {
+    const [infos, setInfos] = useState<Info[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+    
+    useEffect(() => {
+    const fetchInfos = async () => {
+        try {
+            const response = await fetch(process.env.NEXT_PUBLIC_INFO_API_URL as string);
+            if (!response.ok) {
+                throw new Error('ネットワークエラーです');
+            }
+            const data = await response.json();
+            setInfos(data);
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                setError(err.message);
+            } else {
+                setError('不明なエラーが発生しました');
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    fetchInfos();
+}, []);
+
+if (loading) return <div><p className='mb-4'><TextStyle styleType="title">お知らせ</TextStyle></p><p><TextStyle styleType='body1_bold_khaki'>読み込み中…</TextStyle></p></div>;
+if (error) return <div><p className='mb-4'><TextStyle styleType="title">お知らせ</TextStyle></p><p><TextStyle styleType='body1_bold_khaki'>エラーが発生しました: {error}</TextStyle></p></div>;
+
+return (
+    <div>
+        <p className='mb-4'>
+            <TextStyle styleType="title">お知らせ</TextStyle>
+        </p>
+            {infos.map((info) => (
+                <div key={info.番号} className='mb-4'>
+                    <p>
+                        <TextStyle styleType="body1_khaki">{info.投稿日時}</TextStyle>
+                    </p>
+                    <p>
+                        <TextStyle styleType="body1_bold_khaki">{info.タイトル}</TextStyle>
+                    </p>
+                    <p>
+                        <TextStyle styleType="body2">{info.内容}</TextStyle>
+                    </p>
+                </div>
+            ))}
+    </div>
+);
+};
+
+export default Infos;
