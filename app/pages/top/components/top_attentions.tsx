57a4e3c9f655f5ad2e9bde723cfa816a99aee01e
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-05-03 01:50:05 +0900

    [feat]コンポーネントを作成

--- app/pages/top/components/top_attentions.tsx
+++ app/pages/top/components/top_attentions.tsx
@@ -1,12 +1,45 @@
 import Frame from "@/app/components/frame";
 import TextStyle from "@/app/components/text_style";
+import { FaCircleExclamation } from "react-icons/fa6";
 
 const TopAttentions: React.FC = () => {
-    return(
+  return (
+    <Frame pg="none" className="px-[20px] py-6">
+      <TextStyle styleType="section_title" className="text-center pb-6">
+        技大祭を楽しむために
+      </TextStyle>
 
-        <Frame>
-            <TextStyle styleType="section_title" className="text-center">技大祭を楽しむために</TextStyle>
-        </Frame>
-    );
-}
+      <div className="flex justify-start items-center space-x-2 w-full pb-6">
+        <FaCircleExclamation size={32} className="text-main" />
+        <TextStyle styleType="body1_bold_khaki">
+          ご来場の際、<br />お困りの際は案内所まで
+        </TextStyle>
+        </div>
+        <div className="flex justify-start items-center space-x-2 w-full pb-6">
+        <FaCircleExclamation size={32} className="text-main" />
+        <TextStyle styleType="body1_bold_khaki">
+          アルコール提供は<br />リストバンドが必要
+        </TextStyle>
+        </div>
+        <div className="flex justify-start items-center space-x-2 w-full pb-6">
+        <FaCircleExclamation size={32} className="text-main" />
+        <TextStyle styleType="body1_bold_khaki">
+          大学構内では食事が禁止
+        </TextStyle>
+        </div>
+        <div className="flex justify-start items-center space-x-2 pb-6">
+        <FaCircleExclamation size={32} className="text-main" />
+        <TextStyle styleType="body1_bold_khaki">
+          構内は全面禁煙
+        </TextStyle>
+        </div>
+        <div className="flex justify-start items-center space-x-2">
+        <FaCircleExclamation size={32} className="text-main" />
+        <TextStyle styleType="body1_bold_khaki">
+          公共交通機関をご利用<br />ください
+        </TextStyle>
+        </div>
+    </Frame>
+  );
+};
 export default TopAttentions;
