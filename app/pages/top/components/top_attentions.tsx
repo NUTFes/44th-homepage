4dd326d409f779102e66a7a7d29527534a613281
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-05-03 01:00:06 +0900

    [feat]top_attention.tsxを作成

---
+++ app/pages/top/components/top_attentions.tsx
@@ -0,0 +1,12 @@
+import Frame from "@/app/components/frame";
+import TextStyle from "@/app/components/text_style";
+
+const TopAttentions: React.FC = () => {
+    return(
+
+        <Frame>
+            <TextStyle styleType="section_title" className="text-center">技大祭を楽しむために</TextStyle>
+        </Frame>
+    );
+}
+export default TopAttentions;
--- app/pages/top/page.tsx
+++ app/pages/top/page.tsx
@@ -3,11 +3,13 @@ import TextStyle from "@/app/components/text_style";
 import Frame from "@/app/components/frame";
 import Icon from "@/public/icon/44thlogo.png";
 import Image from "next/image";
+import TopAttentions from "./components/top_attentions";
 
 export default function TopPage() {
   return (
     <div>
       <h1>Top Page</h1>
+      <TopAttentions/>
     </div>
   );
 }
