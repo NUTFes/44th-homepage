fd098d6651541a10ae38d5ac53f01d0cca0bed74
mtureit <mtureit@gmail.com>
2025-05-05 11:59:35 +0900

    page.tsxの変更を取り消し

--- app/pages/top/page.tsx
+++ app/pages/top/page.tsx
@@ -3,15 +3,11 @@ import TextStyle from "@/app/components/text_style";
 import Frame from "@/app/components/frame";
 import Icon from "@/public/icon/44thlogo.png";
 import Image from "next/image";
-import RecruitmentSponsors from "./components/recruitment_sponsors";
 
 export default function TopPage() {
   return (
     <div>
       <h1>Top Page</h1>
-      <div className="my-8 max-w-md mx-auto">
-        <RecruitmentSponsors />
-      </div>
     </div>
   );
 }
