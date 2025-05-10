bec8550852ea036a73f8637779dfecd4cbc90924
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-05-10 18:01:15 +0900

    [fix]shadow_darkをついか

--- app/pages/top/components/top_carousel.tsx
+++ app/pages/top/components/top_carousel.tsx
@@ -2,13 +2,33 @@ import Carousel from '@/app/components/carousel';
 
 const TopCarousel: React.FC = () => {
   return (
-      <Carousel autoSlide={true}>
-        <img src="/carousel_top/top_5_1.jpg" alt="昨年度の様子_1" className="w-full h-full" />
-        <img src="/carousel_top/top_5_2.jpg" alt="昨年度の様子_2" className="w-full h-full" />
-        <img src="/carousel_top/top_5_3.jpg" alt="昨年度の様子_3" className="w-full h-full" />
-        <img src="/carousel_top/top_5_4.jpg" alt="昨年度の様子_4" className="w-full h-full" />
-        <img src="/carousel_top/top_5_5.jpg" alt="昨年度の様子_5" className="w-full h-full" />
-      </Carousel>
+    <Carousel autoSlide={true}>
+      <img
+        src="/carousel_top/top_5_1.jpg"
+        alt="昨年度の様子_1"
+        className="w-full h-full  shadow_dark"
+      />
+      <img
+        src="/carousel_top/top_5_2.jpg"
+        alt="昨年度の様子_2"
+        className="w-full h-full shadow_dark"
+      />
+      <img
+        src="/carousel_top/top_5_3.jpg"
+        alt="昨年度の様子_3"
+        className="w-full h-full shadow_dark"
+      />
+      <img
+        src="/carousel_top/top_5_4.jpg"
+        alt="昨年度の様子_4"
+        className="w-full h-full shadow_dark"
+      />
+      <img
+        src="/carousel_top/top_5_5.jpg"
+        alt="昨年度の様子_5"
+        className="w-full h-full shadow_dark"
+      />
+    </Carousel>
   );
 };
 
