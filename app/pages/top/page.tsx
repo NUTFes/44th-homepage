fbd9189ced3736f720243717c48fc6294e10da06
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-05-10 16:56:13 +0900

    [feat]~お知らせまでをトップページに追加

--- app/pages/top/page.tsx
+++ app/pages/top/page.tsx
@@ -1,13 +1,32 @@
-import React from "react";
-import TextStyle from "@/app/components/text_style";
-import Frame from "@/app/components/frame";
-import Icon from "@/public/icon/44thlogo.png";
-import Image from "next/image";
+import BackFrame from '@/app/components/back_frame';
+import Image from 'next/image';
+import Infos from './components/Infos';
+import TopCarousel from './components/top_carousel';
 
 export default function TopPage() {
   return (
     <div>
-      <h1>Top Page</h1>
+      <div className="flex justify-center">
+        <Image
+          src="/logo/title_logo.svg"
+          alt="44th_logo"
+          width={200}
+          height={200}
+        />
+      </div>
+      <div className="flex justify-center">
+        <Image
+          src="/logo/title_date.svg"
+          alt="44th_date"
+          width={200}
+          height={200}
+        />
+      </div>
+      <BackFrame>
+        <TopCarousel/>
+        <Infos/>
+
+        </BackFrame>
     </div>
   );
 }
