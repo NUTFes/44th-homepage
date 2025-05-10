11b9b38ff7741b29b6b2c889c4aea1422e3bef38
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-05-10 22:24:20 +0900

    [fix]Imageコンポーネントに置き換え

--- app/pages/top/components/top_carousel.tsx
+++ app/pages/top/components/top_carousel.tsx
@@ -1,32 +1,43 @@
 import Carousel from '@/app/components/carousel';
+import Image from 'next/image';
 
 const TopCarousel: React.FC = () => {
   return (
     <Carousel autoSlide={true}>
-      <img
+      <Image
         src="/carousel_top/top_5_1.jpg"
         alt="昨年度の様子_1"
-        className="w-full h-full  shadow_dark"
+        width={1920} // 必要に応じて画像の幅を指定
+        height={1080} // 必要に応じて画像の高さを指定
+        className="w-full h-full shadow_dark object-cover"
       />
-      <img
+      <Image
         src="/carousel_top/top_5_2.jpg"
         alt="昨年度の様子_2"
-        className="w-full h-full shadow_dark"
+        width={1920}
+        height={1080}
+        className="w-full h-full shadow_dark object-cover"
       />
-      <img
+      <Image
         src="/carousel_top/top_5_3.jpg"
         alt="昨年度の様子_3"
-        className="w-full h-full shadow_dark"
+        width={1920}
+        height={1080}
+        className="w-full h-full shadow_dark object-cover"
       />
-      <img
+      <Image
         src="/carousel_top/top_5_4.jpg"
         alt="昨年度の様子_4"
-        className="w-full h-full shadow_dark"
+        width={1920}
+        height={1080}
+        className="w-full h-full shadow_dark object-cover"
       />
-      <img
+      <Image
         src="/carousel_top/top_5_5.jpg"
         alt="昨年度の様子_5"
-        className="w-full h-full shadow_dark"
+        width={1920}
+        height={1080}
+        className="w-full h-full shadow_dark object-cover"
       />
     </Carousel>
   );
