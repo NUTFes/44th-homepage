7ba000b59526bb2ca4a4471b9d50de56296e0041
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-05-10 22:35:47 +0900

    [feat]お知らせのエラー文の文字色を指定

--- app/pages/top/components/Infos.tsx
+++ app/pages/top/components/Infos.tsx
@@ -1,38 +1,38 @@
 import InfosClient from './InfosClient';
 
 interface Info {
-    番号: string;
-    投稿日時: string;
-    タイトル: string;
-    内容: string;
+  番号: string;
+  投稿日時: string;
+  タイトル: string;
+  内容: string;
 }
 
 const Infos = async () => {
-    try {
-        const res = await fetch(process.env.NEXT_PUBLIC_INFO_API_URL as string, {
-            cache: 'no-store', 
-});
+  try {
+    const res = await fetch(process.env.NEXT_PUBLIC_INFO_API_URL as string, {
+      cache: 'no-store',
+    });
 
     if (!res.ok) {
-        throw new Error('ネットワークエラーです');
+      throw new Error('ネットワークエラーです');
     }
 
     const infos: Info[] = await res.json();
-    
-    return (
-    <InfosClient infos={infos} />
-    );
-} catch (error: unknown) {
+
+    return <InfosClient infos={infos} />;
+  } catch (error: unknown) {
     const errorMessage =
-    error instanceof Error ? error.message : '不明なエラー';
+      error instanceof Error ? error.message : '不明なエラー';
 
     return (
-    <div>
-        <p className="mb-4"><strong>お知らせ</strong></p>
-        <p>エラーが発生しました: {errorMessage}</p>
-    </div>
+      <div>
+        <p className="mb-4 text-font_main">
+          <strong>お知らせ</strong>
+        </p>
+        <p className="text-font_main">エラーが発生しました: {errorMessage}</p>
+      </div>
     );
-}
+  }
 };
 
 export default Infos;
