2ea0abefbdd0ea499dd81659162643e409c40e87
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-05-13 01:12:07 +0900

    [feat]お知らせテキストの右パディングを指定

--- app/pages/top/components/InfosClient.tsx
+++ app/pages/top/components/InfosClient.tsx
@@ -23,26 +23,28 @@ const InfosClient: React.FC<Props> = ({ infos }) => {
         <p className="text-center">
           <TextStyle styleType="section_title">お知らせ</TextStyle>
         </p>
-        <div className="overflow-y-auto max-h-96">
-          {infos.map((info) => (
-            <div key={info.番号}>
-              <p>
-                <TextStyle styleType="body3_khaki">{info.投稿日時}</TextStyle>
-              </p>
-              <p>
-                <TextStyle styleType="body2_bold_khaki">
-                  {info.タイトル}
-                </TextStyle>
-              </p>
-              <p>
-                <TextStyle styleType="body3">{info.内容}</TextStyle>
-              </p>
+        <div className="overflow-y-auto max-h-96 scrollbar-custom">
+          <div className="pr-2">
+            {infos.map((info) => (
+              <div key={info.番号}>
+                <p>
+                  <TextStyle styleType="body3_khaki">{info.投稿日時}</TextStyle>
+                </p>
+                <p>
+                  <TextStyle styleType="body2_bold_khaki">
+                    {info.タイトル}
+                  </TextStyle>
+                </p>
+                <p>
+                  <TextStyle styleType="body3">{info.内容}</TextStyle>
+                </p>
 
-              <div className="py-4">
-                <Line className="border-main" />
+                <div className="py-4">
+                  <Line className="border-main" />
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </Frame>
     </div>
