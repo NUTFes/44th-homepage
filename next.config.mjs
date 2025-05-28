55f25e259057424ed815afd38d1afc9707e14f79
purin_gidai <24.m.honma.nutfes@gmail.com>
2025-05-28 17:54:41 +0900

    [fix]画像最適化をしなくする設定といらないコメントアウトを削除

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // もし他に設定があれば、それはそのまま残してください
-  // ↓↓↓ この部分を追記 ↓↓↓
-  images: {
-    unoptimized: true, // 画像の最適化を無効化
-  },
   webpack(config) {
     config.cache = false;
     return config;
   },
-  // ↑↑↑ この部分を追記 ↑↑↑
 };
 
 export default nextConfig;
