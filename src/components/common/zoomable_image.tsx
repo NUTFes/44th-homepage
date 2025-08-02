753e4bd8a09031dbd62fd163b3012746246fe510
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-08-03 04:42:26 +0900

    [feat]影を追加

--- src/components/common/zoomable_image.tsx
+++ src/components/common/zoomable_image.tsx
@@ -6,19 +6,14 @@ interface ZoomableImageProps {
 
 function ZoomableImage({ children }: ZoomableImageProps) {
   return (
-    
     <TransformWrapper initialScale={1}>
       <TransformComponent>
         {/* TransformComponent の内部に position: relative の div を追加 */}
-      <div className="border-2 border-main rounded-sm">
-
-
-{children}
-</div>
-
+        <div className="border-2 border-main rounded-sm shadou_button">
+          {children}
+        </div>
       </TransformComponent>
     </TransformWrapper>
-
   );
 }
 
