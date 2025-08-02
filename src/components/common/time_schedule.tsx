eae11949778ae35c92d058f4c6f244e4c9b3883d
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-08-03 04:31:09 +0900

    [feat]文字を追加

--- src/components/common/time_schedule.tsx
+++ src/components/common/time_schedule.tsx
@@ -1,30 +1,64 @@
 'use client';
-import Image from 'next/image';
 import Carousel from './carousel';
-import ZoomableImage from './zoomable_image';
 import ImageModal from './image_modal';
+import ZoomableImage from './zoomable_image';
 const TimeSchedule: React.FC = () => {
   return (
-   <div>
-    <div className="body2 text-center text-font_khaki"><p>ピンチ操作で画像を拡大／縮小、</p><p>タップで全画面表示に切り替わります</p></div>
-   <div className="border-2 border-main rounded-sm">
-   <Carousel autoSlide={false} >
-<ZoomableImage>
-  
-  <ImageModal src="/images/time_schedule/time_1sunny.png" alt="1日目晴れ" width={3413} height={1942}/>
-</ZoomableImage>
-<ZoomableImage>
-  <ImageModal src="/images/time_schedule/time_1rain.png" alt="1日目雨" width={3413} height={1942}/>
-</ZoomableImage>
-<ZoomableImage>
-  <ImageModal src="/images/time_schedule/time_2sunny.png" alt="2日目晴れ" width={3413} height={1942}/>
-</ZoomableImage>
-<ZoomableImage>
-  <ImageModal src="/images/time_schedule/time_2rain.png" alt="2日目雨" width={3413} height={1942}/>
-</ZoomableImage>
-</Carousel>
-</div>
-</div>
+    <div>
+      <div className="body2 text-center text-font_khaki">
+        <p>ピンチ操作で画像を拡大／縮小、</p>
+        <p>タップで全画面表示に切り替わります</p>
+      </div>
+      <div className="border-2 border-main rounded-sm">
+        <Carousel autoSlide={false}>
+          <div className="px-4">
+            <p className="font-bold text-font_khaki">1日目（8/13）スケジュール・晴れ</p>
+            <ZoomableImage>
+              <ImageModal
+                src="/images/time_schedule/time_1sunny.png"
+                alt="1日目晴れ"
+                width={3413}
+                height={1942}
+              />
+            </ZoomableImage>
+          </div>
+          <div className="px-4">
+            <p className="font-bold text-font_khaki">1日目（8/13）スケジュール・雨</p>
+          <ZoomableImage>
+            <ImageModal
+              src="/images/time_schedule/time_1rain.png"
+              alt="1日目雨"
+              width={3413}
+              height={1942}
+            />
+          </ZoomableImage>
+          </div>
+          <div className="px-4">
+            <p className="font-bold text-font_khaki">2日目（8/14）スケジュール・晴れ</p>
+          <ZoomableImage>
+            
+            <ImageModal
+              src="/images/time_schedule/time_2sunny.png"
+              alt="2日目晴れ"
+              width={3413}
+              height={1942}
+            />
+          </ZoomableImage>
+          </div>
+          <div className="px-4">
+            <p className="font-bold text-font_khaki">2日目（8/14）スケジュール・雨</p>
+          <ZoomableImage>
+            <ImageModal
+              src="/images/time_schedule/time_2rain.png"
+              alt="2日目雨"
+              width={3413}
+              height={1942}
+            />
+          </ZoomableImage>
+          </div>
+        </Carousel>
+      </div>
+    </div>
   );
 };
 export default TimeSchedule;
