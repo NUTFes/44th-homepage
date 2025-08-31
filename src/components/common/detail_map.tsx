27672eefa6e3cd84afc078ebfc25e9bef86acadd
oniwawa <s243336@stn.nagaokaut.ac.jp>
2025-08-31 20:04:10 +0900

    [feat]詳細にマップ表示されるよう修正

--- src/components/common/detail_map.tsx
+++ src/components/common/detail_map.tsx
@@ -1,14 +1,29 @@
 'use client';
 
+import Image from 'next/image';
 import Frame from './frame';
+import LinkButton from './link_button';
 import TextStyle from './text_style';
+import KougiMap from '../map/kougi_map';
 
 type DetailMapProps = {
   location: string;
   roomNumber?: string;
 };
 
+const locationImageMap: Record<string, string> = {
+  講義棟エリア: '/images/map/area_5jimutou.png',
+  事務棟エリア: '/images/map/area_5jimutou.png',
+  図書館エリア: '/images/map/area_4tosyokan.png',
+  電気棟エリア: '/images/map/area_3denki.png',
+  機械建設棟エリア: '/images/map/area_1kikai.png',
+  屋外ステージエリア: '/images/map/area_2okugai.png',
+  キッチンカーエリア: '/images/map/area_6kitchin.png',
+};
+
 const DetailMap = ({ location, roomNumber }: DetailMapProps) => {
+  const imageSrc = locationImageMap[location];
+
   return (
     <div>
       <Frame>
@@ -23,15 +38,22 @@ const DetailMap = ({ location, roomNumber }: DetailMapProps) => {
             {roomNumber}
           </TextStyle>
         )}
-        {/* Map image placeholder */}
-        <div className="bg-gray-200 w-full h-64 flex items-center justify-center mb-4">
-          coming soon
+
+        <div className="mb-4">
+          {location === '講義棟エリア' ? (
+            <KougiMap />
+          ) : imageSrc ? (
+            <Image src={imageSrc} alt={`${location}の地図`} width={1000} height={1000} />
+          ) : (
+            <span>coming soon</span>
+          )}
         </div>
-        {/* <LinkButton href="/map" className="bg-second">
+
+        <LinkButton href="/map" className="bg-second">
           <TextStyle styleType="body1" className="text-white">
             マップページへ
           </TextStyle>
-        </LinkButton> */}
+        </LinkButton>
       </Frame>
     </div>
   );
--- src/components/map/area_map.tsx
+++ src/components/map/area_map.tsx
@@ -1,56 +1,66 @@
 'use client';
+
 import Carousel from '../common/carousel';
 import ImageModal from '../common/image_modal';
 import ZoomableImage from '../common/zoomable_image';
+
 const AreaMap: React.FC = () => {
   return (
     <div className="text-center flex flex-col gap-y-4">
       <Carousel autoSlide={false}>
-          <ZoomableImage>
-            <ImageModal
-              src="/images/map/area_1kikai.png"
-              alt="全体マップ"
-              width={2700}
-              height={3400}
-            />
-          </ZoomableImage>
+        <ZoomableImage>
+          <ImageModal
+            src="/images/map/area_1kikai.png"
+            alt="機械棟エリア"
+            width={2700}
+            height={3400}
+          />
+        </ZoomableImage>
 
-          <ZoomableImage>
-            <ImageModal
-              src="/images/map/area_2okugai.png"
-              alt="全体マップ_謎解き"
-              width={2700}
-              height={3400}
-            />
-          </ZoomableImage>
+        <ZoomableImage>
+          <ImageModal
+            src="/images/map/area_2okugai.png"
+            alt="屋外ステージエリア"
+            width={2700}
+            height={3400}
+          />
+        </ZoomableImage>
 
-          <ZoomableImage>
-            <ImageModal
-              src="/images/map/area_3denki.png"
-              alt="全体マップ_スタンプラリー"
-              width={2700}
-              height={3400}
-            />
-          </ZoomableImage>
-                    <ZoomableImage>
-            <ImageModal
-              src="/images/map/area_4tosyokan.png"
-              alt="全体マップ_スタンプラリー"
-              width={2700}
-              height={3400}
-            />
-          </ZoomableImage>
-                    <ZoomableImage>
-            <ImageModal
-              src="/images/map/area_5jimutou.png"
-              alt="全体マップ_スタンプラリー"
-              width={2700}
-              height={3400}
-            />
-          </ZoomableImage>
+        <ZoomableImage>
+          <ImageModal
+            src="/images/map/area_3denki.png"
+            alt="電気棟エリア"
+            width={2700}
+            height={3400}
+          />
+        </ZoomableImage>
+        <ZoomableImage>
+          <ImageModal
+            src="/images/map/area_4tosyokan.png"
+            alt="図書館棟エリア"
+            width={2700}
+            height={3400}
+          />
+        </ZoomableImage>
+        <ZoomableImage>
+          <ImageModal
+            src="/images/map/area_5jimutou.png"
+            alt="事務棟エリア"
+            width={2700}
+            height={3400}
+          />
+        </ZoomableImage>
+
+        <ZoomableImage>
+          <ImageModal
+            src="/images/map/area_6kitchin.png"
+            alt="キッチンカーエリア"
+            width={2700}
+            height={3400}
+          />
+        </ZoomableImage>
       </Carousel>
     </div>
   );
 };
 export default AreaMap;
-
